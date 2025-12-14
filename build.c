75525586ddac658a2a05db40adfa5563e9c48c96
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-12-14 18:21:16 +0300

    chore: migrated tgbot code from rrwebsite

---
+++ build.c
@@ -0,0 +1,32 @@
+#define NOB_STRIP_PREFIX
+
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+#undef NOB_IMPLEMENTATION
+
+int main(int argc, char** argv) {
+
+	NOB_GO_REBUILD_URSELF(argc, argv);
+	
+	Cmd cmd = {0};
+
+	if (!nob_mkdir_if_not_exists("out")) return 1;
+
+	if (!nob_file_exists("credentials.h")) {
+		nob_write_entire_file("credentials.h", "", 0);
+	}
+
+	if (needs_rebuild1("out/mongoose.o", "3rd_party/mongoose/mongoose.c")) {
+		cmd_append(&cmd,
+				"cc", "-c", "3rd_party/mongoose/mongoose.c", "-o", "out/mongoose.o",
+				"-DMG_TLS=MG_TLS_BUILTIN");
+		if (!cmd_run(&cmd)) return 1;
+	}
+
+	cmd_append(&cmd, "cc", "main.c", "-o", "out/rrtgbot");
+	cmd_append(&cmd, "out/mongoose.o");
+	cmd_append(&cmd, "-I./3rd_party");
+	if (!cmd_run(&cmd)) return 1;
+
+	return 0;
+}
---
+++ main.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <signal.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <time.h>
+
+#include "mongoose/mongoose.h"
+
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+#undef NOB_IMPLEMENTATION
+
+#define FLAG_IMPLEMENTATION
+#include "flag.h"
+#undef FLAG_IMPLEMENTATION
+
+#include "credentials.h" /* Created by user */
+
+#define TGBOT_IMPLEMENTATION
+#include "tgbot.h"
+#undef TGBOT_IMPLEMENTATION
+
+// --- UTILS ---
+
+void RandomBytes(void *buf, size_t len) {
+	int fd = open("/dev/urandom", O_RDONLY);
+	assert(fd >= 0);
+	size_t n = read(fd, buf, len);
+	assert(n == len);
+	close(fd);
+}
+
+// --- APP ---
+
+struct Flags {
+	// ...
+} flags;
+
+void FlagsParse(int argc, char** argv) {
+	mg_log_set(MG_LL_NONE);
+
+	bool* f_help = flag_bool("help", 0, "help");
+	uint64_t* f_ll = flag_uint64("log-level", 0, "none, error, info, debug, verbose (0, 1, 2, 3, 4)");
+
+	if (!flag_parse(argc, argv)) {
+    flag_print_options(stdout);
+		flag_print_error(stderr);
+		exit(1);
+	}
+
+	if (*f_help) {
+    flag_print_options(stdout);
+		exit(0);
+	}
+
+	mg_log_set(*f_ll);
+}
+
+// --- EVENTS ---
+
+// --- MAIN ---
+
+char is_working = 1;
+
+void app_terminate(int sig) { is_working = 0; }
+
+int main(int argc, char* argv[]) {
+	FlagsParse(argc, argv);
+
+	printf("log_level: %d\n", mg_log_level);
+
+	struct mg_mgr mgr;
+	mg_mgr_init(&mgr);
+	TGBotConnect(&mgr);
+
+	signal(SIGINT, app_terminate);
+	signal(SIGTERM, app_terminate);
+	while (is_working) {
+		mg_mgr_poll(&mgr, 1000);
+		TGBotPoll();
+	}
+
+	// Closing
+	mg_mgr_free(&mgr);
+	printf("Server closed.\n");
+
+	return 0;
+}
